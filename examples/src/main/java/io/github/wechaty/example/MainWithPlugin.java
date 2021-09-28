4c28fdacc0ed07870af6fbf304e37464d2fff478
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-09-28 18:32:32 +0800

    update

--- examples/src/main/java/io/github/wechaty/example/MainWithPlugin.java
+++ examples/src/main/java/io/github/wechaty/example/MainWithPlugin.java
@@ -1,13 +1,21 @@
 package io.github.wechaty.example;
 
 import io.github.wechaty.Wechaty;
+import io.github.wechaty.WechatyOptions;
 import io.github.wechaty.plugins.WechatyPlugins;
+import io.github.wechaty.schemas.PuppetOptions;
 
 public class MainWithPlugin {
 
     public static void main(String[] args) {
 
-        Wechaty bot = Wechaty.instance("a5134f66-d585-4fd1-8974-26e1d3302c55")
+        PuppetOptions puppetOptions = new PuppetOptions();
+        puppetOptions.setEndPoint("localhost:8788");
+        puppetOptions.setToken("a5134f66-d585-4fd1-8974-26e1d3302c55");
+        WechatyOptions wechatyOptions = new WechatyOptions();
+        wechatyOptions.setPuppetOptions(puppetOptions);
+
+        Wechaty bot = Wechaty.instance(wechatyOptions)
             .use(WechatyPlugins.ScanPlugin(), WechatyPlugins.DingDongPlugin(null))
             .start(true);
 
